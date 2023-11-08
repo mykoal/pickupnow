9559eb05431121b0ec50d87a3566d82c9e561870
NatanGoldschlag <111074916+NatanGoldschlag@users.noreply.github.com>
2023-11-08 11:30:09 -0500

    Small fix to brackets

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {MainScreen} from './Screens/MainScreen';
-import {PlayScreen} from './Screens/PlayScreen';
+import PlayScreen from './Screens/PlayScreen';
 
 function App() {
   return (
