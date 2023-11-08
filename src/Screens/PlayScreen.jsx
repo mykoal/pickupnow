60ff715d09d57332e755a1a04f02cbe29a6d2187
NatanGoldschlag <111074916+NatanGoldschlag@users.noreply.github.com>
2023-11-08 10:58:14 -0500

    creating the play screen

---
+++ src/Screens/PlayScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PlayScreen = () => {
+  return (
+    <div>
+      <h1>Play Now</h1>
+      {/* Add more content and structure here as needed */}
+    </div>
+  );
+};
+
+export default PlayScreen;
