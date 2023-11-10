7feaf5ab54a68aad75bc7a4aec0f9dc11caab745
aria-eghbali <aria.eghbali@gmail.com>
2023-11-10 13:28:02 -0500

    updated queue screen design

--- src/Screens/QueueScreen.jsx
+++ src/Screens/QueueScreen.jsx
@@ -5,37 +5,60 @@ import { NavbarPlay } from "../Components/NavbarPlay";
 
 const QueueScreen = () => {
   let navigate = useNavigate();
-
+  const navigateToPlay = () => {
+    navigate('/play'); // the other button actions need to be added in (join a team, leave)
+  };
   const leaveQueue = () => {
-    navigate('/play'); // UPDATE
+    // Add functionality to leave the queue here
   };
-  const joinQueue = () => {
-    navigate('/play'); // UPDATE
+
+  // Function to calculate the position of the circle in the queue visualization
+  const calculateCirclePosition = (totalPositions, yourPosition) => {
+    const percentage = (yourPosition / totalPositions) * 100;
+    return percentage + '%';
   };
+
   return (
     <div className="bg-white flex flex-row justify-center w-full">
       <div className="bg-white overflow-hidden w-[393px] h-[852px] relative">
-        <div className="absolute w-[164px] top-[160px] left-[13px] [font-family:'Gabarito',Helvetica] font-bold text-[27px] text-[black] tracking-[0] leading-[normal] whitespace-nowrap">
+        <div className="absolute w-[164px] top-[100px] left-[13px] [font-family:'Gabarito',Helvetica] font-bold text-[27px] text-[black] tracking-[0] leading-[normal] whitespace-nowrap">
           Current Queues
         </div>
-        <div className="absolute w-[354px] h-[98px] top-[192px] left-[19px] bg-[#f3fbef] rounded-[15px] border border-solid border-[#8db580]">
-          <p className="absolute w-[332px] top-[13px] left-[10px] [font-family:'Gabarito',Helvetica] font-normal text-[black] text-[20px] text-center tracking-[0] leading-[normal]">
-            Your position in the queue: #5
+        <div className="absolute w-[354px] h-[160px] top-[135px] left-[19px] bg-[#f3fbef] rounded-[15px] border border-solid border-[#8db580]">
+          <p className="absolute w-[332px] top-[10px] left-[10px] [font-family:'Gabarito',Helvetica] font-normal text-[black] text-[15px] text-center tracking-[0] leading-[normal]">
+            You are currently 
+          </p>
+          <p className="absolute w-[332px] top-[40px] left-[10px] [font-family:'Gabarito',Helvetica] font-bold text-[black] text-[60px] text-center tracking-[0] leading-[normal]">
+            #5
+          </p>
+          <p className="absolute w-[332px] bottom-[10px] left-[10px] [font-family:'Gabarito',Helvetica] font-normal text-[black] text-[15px] text-center tracking-[0] leading-[normal]">
+            In the court Queue 
           </p>
         </div>
-        <div className="absolute w-[354px] h-[280px] top-[310px] left-[19px] bg-[#f3fbef] rounded-[15px] border border-solid border-[#8db580]">
-          <div className="absolute w-[393px] h-[67px] top-[10px] left-[10px] text-[25px] text-[#0f6e42] [font-family:'Gabarito',Helvetica] font-bold">
+        <div className="absolute w-[15px] h-[400px] top-[310px] left-[19px] bg-[#0f6e42] rounded-[5px]">
+          {/* SVG for the adjusted queue visualization */}
+          <svg height="400" width="15">
+            <circle
+              cx="7.5"
+              cy={calculateCirclePosition(10, 5)} // Replace 10 with the total number of positions in the queue and 5 with current positon of player 
+              r="8"
+              fill="#f3fbef"
+            />
+          </svg>
+        </div>
+        <div className="absolute w-[320px] h-[400px] top-[310px] left-[50px] bg-[#f3fbef] rounded-[15px] border border-solid border-[#8db580]">
+          <div className="absolute w-[393px] h-[67px] top-[0px] left-[10px] text-[20px] text-[#0f6e42] [font-family:'Gabarito',Helvetica] font-bold">
             Now Playing
           </div>
-          <div className="absolute w-[393px] h-[67px] top-[60px] left-[10px] text-[20px] text-[black] [font-family:'Gabarito',Helvetica] font-normal">
-            Aria & Max
+          <div className="absolute w-[393px] h-[67px] top-[25px] left-[10px] text-[20px] text-[black] [font-family:'Gabarito',Helvetica] font-normal">
+            Aria & Max 
+          </div> 
+          <div className="absolute w-[393px] h-[67px] top-[85px] left-[10px] text-[20px] text-[#0f6e42] [font-family:'Gabarito',Helvetica] font-bold">
+            Up Next
           </div>
-          <div className="absolute w-[393px] h-[67px] top-[110px] left-[10px] text-[25px] text-[#0f6e42] [font-family:'Gabarito',Helvetica] font-bold">
-            Next Up
-          </div>
-          <div className="absolute w-[393px] h-[67px] top-[160px] left-[10px] text-[20px] text-[black] [font-family:'Gabarito',Helvetica] font-normal">
+          <div className="absolute w-[393px] h-[67px] top-[110px] left-[10px] text-[20px] text-[black] [font-family:'Gabarito',Helvetica] font-normal">
             Ricky and [Open Slot]
-            <button className="bg-[#0f6e42] text-[#f3fbef] text-[20px] rounded-[5px] px-[10px] py-[5px] ml-[10px]" onClick={joinQueue}>
+            <button className="bg-[#0f6e42] text-[#f3fbef] text-[20px] rounded-[5px] px-[10px] py-[5px] ml-[10px]" onClick={navigateToPlay}>
               Join
             </button>
           </div>
@@ -46,16 +69,10 @@ const QueueScreen = () => {
             </button>
           </div>
         </div>
-        <div className="absolute w-[393px] h-[67px] top-0 left-0 bg-[#f3fbef]">
-          <img
-            className="absolute w-[171px] h-[38px] top-[14px] left-[13px] object-cover"
-            alt="Logo long"
-            src="https://c.animaapp.com/Yiudwmn6/img/logo-long-1@2x.png"
-          />
-        </div>
         <NavbarPlay></NavbarPlay>
       </div>
     </div>
   );
 };
+
 export default QueueScreen;
