28e7a26fee711dcf47fffe8440ba0a5bf8dad0ba
Xue Jiang <haoxuej@seas.upenn.edu>
2023-11-10 15:30:07 -0500

    Embedded Gabarito font

--- src/Screens/MainScreen.jsx
+++ src/Screens/MainScreen.jsx
@@ -16,10 +16,10 @@ export const MainScreen = () => {
 	return (
 		<div className="bg-white flex flex-row justify-center w-full">
 			<div className="bg-white overflow-hidden w-[393px] h-[852px] relative">
-				<div className="absolute w-[183px] top-[94px] left-[13px] [font-family:'Gabarito',Helvetica] font-normal text-black text-[35px] tracking-[0] leading-[normal]">
+				<div className="absolute w-[183px] top-[94px] left-[13px] [font-family:Gabarito] font-normal text-black text-[35px] tracking-[0] leading-[normal]">
 					Hi Michael!
 				</div>
-				<div className="absolute w-[164px] top-[160px] left-[13px] [font-family:'Gabarito',Helvetica] font-bold text-black text-[20px] tracking-[0] leading-[normal] whitespace-nowrap">
+				<div className="absolute w-[164px] top-[160px] left-[13px] [font-family:Gabarito] font-bold text-black text-[20px] tracking-[0] leading-[normal] whitespace-nowrap">
 					Current Queues
 				</div>
 				<div className="absolute w-[354px] h-[98px] top-[192px] left-[19px] bg-[#f3fbef] rounded-[15px] border border-solid border-[#8db580]">
@@ -27,11 +27,11 @@ export const MainScreen = () => {
 						className="absolute w-[209px] h-[39px] top-[49px] left-[73px] rounded-[5px]"
 						onClick={navigateToPlay}>
 						<div className="absolute w-[209px] h-[39px] top-0 left-0 bg-[#0f6e42] rounded-[5px]" />
-						<div className="absolute w-[206px] h-[30px] top-[8px] left-[2px] [font-family:'Gabarito',Helvetica] font-bold text-[#f3fbef] text-[20px] text-center tracking-[0] leading-[normal]">
+						<div className="absolute w-[206px] h-[30px] top-[8px] left-[2px] [font-family:Gabarito] font-bold text-[#f3fbef] text-[20px] text-center tracking-[0] leading-[normal]">
 							Play Now
 						</div>
 					</div>
-					<p className="absolute w-[332px] top-[13px] left-[10px] [font-family:'Gabarito',Helvetica] font-normal text-black text-[20px] text-center tracking-[0] leading-[normal]">
+					<p className="absolute w-[332px] top-[13px] left-[10px] [font-family:Gabarito] font-normal text-black text-[20px] text-center tracking-[0] leading-[normal]">
 						You aren’t in any queue right now
 					</p>
 				</div>
