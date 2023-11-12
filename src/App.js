ed6664b2da09291678cb27d36eb70780ba918959
Xue Jiang <haoxuej@seas.upenn.edu>
2023-11-12 03:31:28 -0500

    Updated CreateQueueScreen to a form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from "react";import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import {MainScreen} from './Screens/MainScreen';
-import PlayScreen from './Screens/PlayScreen';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { MainScreen } from "./Screens/MainScreen";
+import PlayScreen from "./Screens/PlayScreen";
 import QueueScreen from "./Screens/QueueScreen";
 import SettingsScreen from "./Screens/SettingsScreen";
 import { Markwood } from "./Screens/Markwood";
-import { CreateQueueScreen } from "./Screens/CreateQueueScreen";
+import CreateQueueScreen from "./Screens/CreateQueueScreen";
 
 function App() {
 	//user profile info default
@@ -22,30 +23,28 @@ function App() {
 	};
 
 	useEffect(() => {
-    // Check if user data is already in local storage
-    const storedUserData = localStorage.getItem("userData");
+		// Check if user data is already in local storage
+		const storedUserData = localStorage.getItem("userData");
 
-    // If user data is not in local storage, set the initial data
-    if (!storedUserData) {
-      localStorage.setItem("userData", JSON.stringify(initUserData));
-    }
-  }, []);
+		// If user data is not in local storage, set the initial data
+		if (!storedUserData) {
+			localStorage.setItem("userData", JSON.stringify(initUserData));
+		}
+	}, []);
 
-
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainScreen />} />
-        <Route path="/play" element={<PlayScreen />} />
-        <Route path="/queue" element={<QueueScreen />} />
-        <Route path="/settings" element={<SettingsScreen />} />
-        <Route path="/markwood" element={<Markwood/>} />
-        <Route path="/create_queue" element={<CreateQueueScreen/>} />
-        {/* Add more routes as needed */}
-      </Routes>
-    </Router>
-  );
-
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<MainScreen />} />
+				<Route path="/play" element={<PlayScreen />} />
+				<Route path="/queue" element={<QueueScreen />} />
+				<Route path="/settings" element={<SettingsScreen />} />
+				<Route path="/markwood" element={<Markwood />} />
+				<Route path="/create_queue" element={<CreateQueueScreen />} />
+				{/* Add more routes as needed */}
+			</Routes>
+		</Router>
+	);
 
 	// State for the counter
 	// const [count, setCount] = useState(0);
--- src/Screens/Markwood.jsx
+++ src/Screens/Markwood.jsx
@@ -3,85 +3,99 @@ import { NavbarPlay } from "../Components/NavbarPlay";
 import { useNavigate } from "react-router-dom";
 
 export const Markwood = () => {
-  let navigate = useNavigate();
+	let navigate = useNavigate();
 	const navigateToCreateQueue = () => {
 		navigate("/create_queue"); // '/create_queue' is the path to your new screen
 	};
 
-  return (
-    <div className="bg-white flex flex-row justify-center w-full">
-      <div className="bg-white w-[394px] h-[852px] relative">
-        <div className="absolute w-[365px] h-[55px] top-[129px] left-[23px]">
-          <div className="absolute w-[254px] h-[55px] top-0 left-[94px] bg-[#f3fbef] rounded-[20px] border-2 border-solid border-[#8db580]" />
-          <div className="w-[164px] top-[14px] left-0 whitespace-nowrap absolute [font-family:'Gabarito-Bold',Helvetica] font-bold text-black text-[20px] tracking-[0] leading-[normal]">
-            Complex:
-          </div>
-          <div className="absolute w-[253px] h-[40px] top-[6px] left-[112px] [font-family:'Gabarito-Bold',Helvetica] font-bold text-black text-[20px] tracking-[0] leading-[normal]">
-            Markwood Playground
-          </div>
-        </div>
-        <p className="top-[462px] absolute w-[311px] left-[40px] [font-family:'Gabarito-Regular',Helvetica] font-normal text-black text-[20px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
-          2 Teams Ahead - 2 hr wait time
-        </p>
-        <div className="absolute w-[354px] h-[190px] top-[248px] left-[21px]">
-          <div className="top-0 rounded-[5px_5px_0px_0px] absolute w-[354px] h-[49px] left-0 bg-[#f3fbef] border-2 border-solid border-[#8db580]" />
-          <div className="absolute w-[332px] h-[40px] top-px left-[11px] [font-family:'Gabarito-Regular',Helvetica] font-normal text-black text-[20px] tracking-[0] leading-[normal]">
-            Night Lighting:
-          </div>
-          <div className="absolute w-[332px] h-[40px] top-px left-[11px] [font-family:'Gabarito-Regular',Helvetica] font-normal text-black text-[20px] tracking-[0] leading-[normal]">
-            Night Lighting:
-          </div>
-          <div className="top-px absolute w-[32px] h-[40px] left-[152px] [font-family:'Gabarito-Bold',Helvetica] font-bold text-black text-[20px] text-center tracking-[0] leading-[normal]">
-            4.0
-          </div>
-          <div className="top-[141px] rounded-[0px_0px_5px_5px] absolute w-[354px] h-[49px] left-0 bg-[#f3fbef] border-2 border-solid border-[#8db580]" />
-          <div className="top-[149px] absolute w-[332px] h-[40px] left-[11px] [font-family:'Gabarito-Regular',Helvetica] font-normal text-black text-[20px] tracking-[0] leading-[normal]">
-            Court Net:
-          </div>
-          <div className="top-[94px] absolute w-[354px] h-[49px] left-0 bg-[#f3fbef] border-2 border-solid border-[#8db580]" />
-          <div className="top-[101px] absolute w-[332px] h-[40px] left-[11px] [font-family:'Gabarito-Regular',Helvetica] font-normal text-black text-[20px] tracking-[0] leading-[normal]">
-            Court surface:
-          </div>
-          <div className="top-[47px] absolute w-[354px] h-[49px] left-0 bg-[#f3fbef] border-2 border-solid border-[#8db580]" />
-          <div className="top-[50px] absolute w-[332px] h-[40px] left-[11px] [font-family:'Gabarito-Regular',Helvetica] font-normal text-black text-[20px] tracking-[0] leading-[normal]">
-            Court lines:
-          </div>
-          <img className="top-[10px] absolute w-[131px] h-[23px] left-[195px]" alt="Group" src="https://cdn.animaapp.com/projects/6548013c7fdc3538947dcc9e/releases/654e98f02540041bb7fc102b/img/group-1@2x.png" />
-          <img className="top-[56px] absolute w-[131px] h-[23px] left-[195px]" alt="Group" src="https://cdn.animaapp.com/projects/6548013c7fdc3538947dcc9e/releases/654e98f02540041bb7fc102b/img/group-2@2x.png" />
-          <img className="top-[154px] absolute w-[131px] h-[23px] left-[195px]" alt="Group" src="https://cdn.animaapp.com/projects/6548013c7fdc3538947dcc9e/releases/654e98f02540041bb7fc102b/img/group-4@2x.png" />
-          <img className="top-[104px] absolute w-[131px] h-[23px] left-[195px]" alt="Group" src="https://cdn.animaapp.com/projects/6548013c7fdc3538947dcc9e/releases/654e98f02540041bb7fc102b/img/group-3@2x.png" />
-          <div className="top-[50px] absolute w-[32px] h-[40px] left-[152px] [font-family:'Gabarito-Bold',Helvetica] font-bold text-black text-[20px] text-center tracking-[0] leading-[normal]">
-            4.2
-          </div>
-          <div className="top-[99px] absolute w-[32px] h-[40px] left-[152px] [font-family:'Gabarito-Bold',Helvetica] font-bold text-black text-[20px] text-center tracking-[0] leading-[normal]">
-            3.5
-          </div>
-          <div className="top-[149px] absolute w-[32px] h-[40px] left-[152px] [font-family:'Gabarito-Bold',Helvetica] font-bold text-black text-[20px] text-center tracking-[0] leading-[normal]">
-            4.5
-          </div>
-        </div>
-        <div className="w-[332px] top-[205px] left-[33px] text-center absolute [font-family:'Gabarito-Bold',Helvetica] font-bold text-black text-[20px] tracking-[0] leading-[normal]">
-          Complex Conditions
-        </div>
-        <div className="absolute w-[295px] h-[39px] top-[502px] left-[48px] rounded-[20px]">
-          <div className="absolute w-[295px] h-[39px] top-0 left-0 bg-[#0f6e42] rounded-[20px]" />
-          <div className="absolute w-[295px] h-[39px] top-0 left-0 bg-[#0f6e42] rounded-[20px]" />
-          <p className="absolute w-[291px] top-[6px] left-[4px] [font-family:'Gabarito-Regular',Helvetica] font-normal text-[#f3fbef] text-[20px] text-center tracking-[0] leading-[normal]"
-          onClick={navigateToCreateQueue}>
-            CREATE QUEUE
-          </p>
-        </div>
-        <p className="top-[566px] absolute w-[311px] left-[40px] [font-family:'Gabarito-Regular',Helvetica] font-normal text-black text-[20px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
-          1 Player looking for a team
-        </p>
-        <div className="absolute w-[295px] h-[39px] top-[605px] left-[50px] rounded-[20px]">
-          <div className="absolute w-[295px] h-[39px] top-0 left-0 bg-[#0f6e42] rounded-[20px]" />
-          <div className="absolute w-[291px] top-[7px] left-[4px] [font-family:'Gabarito-Regular',Helvetica] font-normal text-[#f3fbef] text-[20px] text-center tracking-[0] leading-[normal]">
-            JOIN EXISTING TEAM
-          </div>
-        </div>
-        <NavbarPlay></NavbarPlay>
-      </div>
-    </div>
-  );
+	return (
+		<div className="bg-white flex flex-row justify-center w-full">
+			<div className="bg-white w-[394px] h-[852px] relative">
+				<div className="absolute w-[365px] h-[55px] top-[129px] left-[23px]">
+					<div className="absolute w-[254px] h-[55px] top-0 left-[94px] bg-[#f3fbef] rounded-[20px] border-2 border-solid border-[#8db580]" />
+					<div className="w-[164px] top-[14px] left-0 whitespace-nowrap absolute [font-family:Gabarito] font-bold font-bold text-black text-[20px] tracking-[0] leading-[normal]">
+						Complex:
+					</div>
+					<div className="absolute w-[253px] h-[40px] top-[6px] left-[112px] [font-family:Gabarito] font-bold font-bold text-black text-[20px] tracking-[0] leading-[normal]">
+						Markwood Playground
+					</div>
+				</div>
+				<p className="top-[462px] absolute w-[311px] left-[40px] [font-family:Gabarito] font-normal font-normal text-black text-[20px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+					2 Teams Ahead - 2 hr wait time
+				</p>
+				<div className="absolute w-[354px] h-[190px] top-[248px] left-[21px]">
+					<div className="top-0 rounded-[5px_5px_0px_0px] absolute w-[354px] h-[49px] left-0 bg-[#f3fbef] border-2 border-solid border-[#8db580]" />
+					<div className="absolute w-[332px] h-[40px] top-[12px] left-[11px] [font-family:Gabarito] font-normal font-normal text-black text-[20px] tracking-[0] leading-[normal]">
+						Night Lighting:
+					</div>
+					<div className="top-[12px] absolute w-[32px] h-[40px] left-[152px] [font-family:Gabarito] font-bold font-bold text-black text-[20px] text-center tracking-[0] leading-[normal]">
+						4.0
+					</div>
+					<div className="top-[141px] rounded-[0px_0px_5px_5px] absolute w-[354px] h-[49px] left-0 bg-[#f3fbef] border-2 border-solid border-[#8db580]" />
+					<div className="top-[155px] absolute w-[332px] h-[40px] left-[11px] [font-family:Gabarito] font-normal font-normal text-black text-[20px] tracking-[0] leading-[normal]">
+						Court Net:
+					</div>
+					<div className="top-[94px] absolute w-[354px] h-[49px] left-0 bg-[#f3fbef] border-2 border-solid border-[#8db580]" />
+					<div className="top-[107px] absolute w-[332px] h-[40px] left-[11px] [font-family:Gabarito] font-normal font-normal text-black text-[20px] tracking-[0] leading-[normal]">
+						Court surface:
+					</div>
+					<div className="top-[47px] absolute w-[354px] h-[49px] left-0 bg-[#f3fbef] border-2 border-solid border-[#8db580]" />
+					<div className="top-[58px] absolute w-[332px] h-[40px] left-[11px] [font-family:Gabarito] font-normal font-normal text-black text-[20px] tracking-[0] leading-[normal]">
+						Court lines:
+					</div>
+					<img
+						className="top-[10px] absolute w-[131px] h-[23px] left-[195px]"
+						alt="Group"
+						src="https://cdn.animaapp.com/projects/6548013c7fdc3538947dcc9e/releases/654e98f02540041bb7fc102b/img/group-1@2x.png"
+					/>
+					<img
+						className="top-[56px] absolute w-[131px] h-[23px] left-[195px]"
+						alt="Group"
+						src="https://cdn.animaapp.com/projects/6548013c7fdc3538947dcc9e/releases/654e98f02540041bb7fc102b/img/group-2@2x.png"
+					/>
+					<img
+						className="top-[154px] absolute w-[131px] h-[23px] left-[195px]"
+						alt="Group"
+						src="https://cdn.animaapp.com/projects/6548013c7fdc3538947dcc9e/releases/654e98f02540041bb7fc102b/img/group-4@2x.png"
+					/>
+					<img
+						className="top-[104px] absolute w-[131px] h-[23px] left-[195px]"
+						alt="Group"
+						src="https://cdn.animaapp.com/projects/6548013c7fdc3538947dcc9e/releases/654e98f02540041bb7fc102b/img/group-3@2x.png"
+					/>
+					<div className="top-[58px] absolute w-[32px] h-[40px] left-[152px] [font-family:Gabarito] font-bold font-bold text-black text-[20px] text-center tracking-[0] leading-[normal]">
+						4.2
+					</div>
+					<div className="top-[107px] absolute w-[32px] h-[40px] left-[152px] [font-family:Gabarito] font-bold font-bold text-black text-[20px] text-center tracking-[0] leading-[normal]">
+						3.5
+					</div>
+					<div className="top-[155px] absolute w-[32px] h-[40px] left-[152px] [font-family:Gabarito] font-bold font-bold text-black text-[20px] text-center tracking-[0] leading-[normal]">
+						4.5
+					</div>
+				</div>
+				<div className="w-[332px] top-[205px] left-[33px] text-center absolute [font-family:Gabarito] font-bold font-bold text-black text-[20px] tracking-[0] leading-[normal]">
+					Complex Conditions
+				</div>
+				<div className="absolute w-[295px] h-[39px] top-[502px] left-[48px] rounded-[20px]">
+					<div className="absolute w-[295px] h-[39px] top-0 left-0 bg-[#0f6e42] rounded-[20px]" />
+					<div className="absolute w-[295px] h-[39px] top-0 left-0 bg-[#0f6e42] rounded-[20px]" />
+					<p
+						className="absolute w-[291px] top-[6px] left-[4px] [font-family:Gabarito] font-normal font-normal text-[#f3fbef] text-[20px] text-center tracking-[0] leading-[normal]"
+						onClick={navigateToCreateQueue}>
+						CREATE QUEUE
+					</p>
+				</div>
+				<p className="top-[566px] absolute w-[311px] left-[40px] [font-family:Gabarito] font-normal font-normal text-black text-[20px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+					1 Player looking for a team
+				</p>
+				<div className="absolute w-[295px] h-[39px] top-[605px] left-[50px] rounded-[20px]">
+					<div className="absolute w-[295px] h-[39px] top-0 left-0 bg-[#0f6e42] rounded-[20px]" />
+					<div className="absolute w-[291px] top-[7px] left-[4px] [font-family:Gabarito] font-normal font-normal text-[#f3fbef] text-[20px] text-center tracking-[0] leading-[normal]">
+						JOIN EXISTING TEAM
+					</div>
+				</div>
+				<NavbarPlay></NavbarPlay>
+			</div>
+		</div>
+	);
 };
